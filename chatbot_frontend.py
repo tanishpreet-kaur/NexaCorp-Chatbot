0fb13d441df5b4f2e538d17326583abf4467d47d
Tanishpreet Kaur <tanish.kaur2004@gmail.com>
2026-05-20 11:26:16 +0000

    changes

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,36 +1,28 @@
 import streamlit as st
 from chatbot_backend import chatbot
 
-# ===================== Page Config =====================
+# Page Config 
 st.set_page_config(
     page_title="NexaCorp Chatbot",
     layout="wide"
 )
 
-# ===================== Sidebar =====================
-
+# Sidebar 
 with st.sidebar:
-
-    st.title("🤖 NexaCorp Assistant")
-
+    st.title("NexaCorp Assistant")
     st.divider()
+    st.subheader("Chat History")
 
-    st.subheader("Past Chats")
-
-
-# ===================== Main UI =====================
+# Main UI
 st.title("NexaCorp Global")
-st.caption(
-    "Ask your HR-related questions here"
-)
+st.caption("Ask your HR-related questions here")
 
 # Display chat history
 for msg in st.session_state.message_history:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
-
-# ===================== User Input =====================
+# User Input 
 prompt = st.chat_input(
-    "Ask something..."
+    "Type here..."
 )
